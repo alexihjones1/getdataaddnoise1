9573d966c8e00b2fc04519b40827eb531b2bdc08
alexihjones1 <127150204+alexihjones1@users.noreply.github.com>
2023-04-27 09:56:25 -0500

    Update GetDataAsset.R

--- R/GetDataAsset.R
+++ R/GetDataAsset.R
@@ -2,7 +2,9 @@ GetDataAsset <- function() {
   # Download the file from Dropbox
   url <- "https://www.dropbox.com/s/5mzc60tbh09ew49/lalonde%20nsw.csv.zip?dl=1"
   download.file(url, destfile = "data/lalonde nsw.zip")
+  #Unzip file
   unzip("data/lalonde_nsw.zip", exdir = "data")
   data <- read.csv("data/lalonde nsw.csv")
+  #Return data
   return(data)
 }
